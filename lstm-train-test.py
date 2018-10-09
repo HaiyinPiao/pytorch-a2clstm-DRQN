e5edf70e229f3fc078e380c31d95ab19c2cdf623
haiyinpiao <haiyinpiao@qq.com>
2018-10-09 18:14:24 +0800

    change training mode to batch train, encapsulating time-series inputs as one batch tensor sended to GPU skill got

--- lstm-train-test.py
+++ lstm-train-test.py
@@ -4,27 +4,29 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 
+BATCH = 100;
+TIMESTEP = 10;
+HIDDEN = 3;
+EPOCHS = 50;
+
 class rnn_wrapper(nn.Module):
 
     def __init__(self):
         super(rnn_wrapper, self).__init__()
-        self.lstm = nn.LSTM(3, 3)  # 输入维度是3, 输出维度也是3
-        self.fc = nn.Linear(3,1)
+        self.lstm = nn.LSTM(input_size=HIDDEN, hidden_size=HIDDEN, batch_first = True)  # 输入维度是3, 输出维度也是3
+        self.fc = nn.Linear(HIDDEN,1)
 
     def forward(self, x, hidden):
         x, hidden = self.lstm(x, hidden)
-        x = F.relu(self.fc(x))
+        x = self.fc(x)
         return x, hidden
 
 
 
 torch.manual_seed(1)
 
-inputs = [torch.randn(1, 3) for _ in range(10)] # 构造一个长度为10的序列
-print('Inputs:',inputs)
-
 # 初始化隐藏状态
-hidden_init = (torch.randn(1, 1, 3), torch.randn(1, 1, 3))
+hidden_init = (torch.randn(1, BATCH, HIDDEN), torch.randn(1, BATCH, HIDDEN))
 hidden = hidden_init
 # print('Hidden:',hidden)
 
@@ -33,29 +35,22 @@ rnn = rnn_wrapper()
 loss_function=nn.MSELoss()
 optimizer=optim.SGD(rnn.parameters(), lr=0.1)
 
-target = torch.FloatTensor([i for i in range(10)])
-# print(target)
+# target construct
+q = [(i*np.ones((BATCH,1))).tolist() for i in range(TIMESTEP)]
+target = torch.FloatTensor(q).transpose(0,1)
 
-for epoch in range(500):
+# input construct
+k = np.random.randn(TIMESTEP,HIDDEN).tolist()
+inn = torch.FloatTensor([ k for i in range(BATCH)])
+
+for epoch in range(EPOCHS):
     rnn.zero_grad()
     hidden = hidden_init
 
-    q = []
-    for i in inputs:
-        # print(hidden[0])
-        out, hidden = rnn(i.view(1, 1, -1), hidden)
-        q.append(out)
+    out, hidden = rnn(inn, hidden)
 
-    m = torch.stack(q, dim=1)
-    
-    m=m.view(10,-1)
-    target=target.view(10,-1)
-    print(m)
-
-    loss=loss_function(m, target)
+    loss=loss_function(out, target)
     print('Loss:',loss.item())
     loss.backward()
     
-    
-    # print( lstm.all_weights )
     optimizer.step()
